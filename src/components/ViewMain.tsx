0d61b877f52129ae51eb8b574e7d4c234acaf13a
Chansu Kim <ckstn0777@naver.com>
2023-05-11 12:57:51 +0900

    feat: view transitions api 없이 구현

--- src/components/ViewMain.tsx
+++ src/components/ViewMain.tsx
@@ -1,17 +1,36 @@
 import { ViewItem } from "../types";
 
 type ViewMainProps = {
+  mainRef: React.RefObject<HTMLImageElement>;
+  futureMainRef: React.RefObject<HTMLImageElement>;
   item: ViewItem;
+  futureItem: ViewItem | undefined;
 };
 
-export default function ViewMain({ item }: ViewMainProps) {
+export default function ViewMain({
+  mainRef,
+  futureMainRef,
+  item,
+  futureItem,
+}: ViewMainProps) {
+  console.log(item);
   return (
     <section className="basis-7/12">
-      <img
-        src={item.img}
-        alt="view main img"
-        className="max-h-80 w-full object-cover"
-      />
+      <div className="relative h-80">
+        <img
+          src={item.img}
+          alt="view main img"
+          className="absolute top-0 left-0 max-h-80 w-full object-cover z-10"
+          ref={mainRef}
+        />
+
+        <img
+          src={futureItem?.img}
+          alt="view main img"
+          className="absolute top-0 left-0 max-h-80 w-full object-cover"
+          ref={futureMainRef}
+        />
+      </div>
 
       <h3 className="text-xl my-4 text-left">{item.title}</h3>
       <hr />
--- src/pages/Example1/index.tsx
+++ src/pages/Example1/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ViewAsideList from "../../components/ViewAsideList";
 import ViewMain from "../../components/ViewMain";
 import { ViewItem } from "../../types";
@@ -31,15 +31,48 @@ const items: ViewItem[] = [
 ];
 
 export default function Example1() {
-  const [currentViewItem, setCurrentViewItem] = useState(items[0]);
+  const [currentViewItem, setCurrentViewItem] = useState(items[0]); // 현재 아이템
+  const [futureViewItem, setFutureViewItem] = useState<ViewItem>(); // 바뀔 아이템
+
+  const mainRef = useRef<HTMLImageElement>(null); // 현재(이제는 과거) 보이는 요소
+  const futureMainRef = useRef<HTMLImageElement>(null); // 미래(이제는 현재) 보이는 요소
 
   const handleClick = (item: ViewItem) => {
-    setCurrentViewItem(item);
+    setFutureViewItem(item);
   };
+
+  useEffect(() => {
+    const main = mainRef.current;
+    const futureMain = futureMainRef.current;
+
+    if (futureViewItem && main && futureMain) {
+      // 현재(이제는 과거) 보이는 요소는 fade-out 되어 화면에서 사라집니다.
+      main.animate([{ opacity: 0 }], { duration: 1000 });
+      // 미래(이제는 현재) 보이는 요소는 fade-in 되어 화면에 보여집니다.
+      futureMain.animate([{ opacity: 1 }], { duration: 1000 });
+
+      // 애니메이션이 종료되기 전 미리 미래요소를 현재요소로 교체 합니다.
+      setTimeout(() => {
+        setCurrentViewItem(futureViewItem);
+      }, 950);
+
+      // 미래 요소는 undefined로 초기화 합니다.
+      setTimeout(() => {
+        setFutureViewItem(undefined);
+      }, 1000);
+    }
+  }, [futureViewItem]);
 
   return (
     <main className="flex flex-col gap-6 md:w-[760px] md:flex-row">
-      <ViewMain item={currentViewItem} />
+      <ViewMain
+        mainRef={mainRef}
+        futureMainRef={futureMainRef}
+        // 현재 데이터가 화면에 렌더링 됩니다.
+        item={currentViewItem}
+        // 미래에 보일 데이터가 있다면 화면에 보이진 않지만 렌더링 됩니다.
+        futureItem={futureViewItem}
+      />
       <ViewAsideList items={items} onClick={handleClick} />
     </main>
   );
